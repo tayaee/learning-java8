6ea65e5a4765cb58acda27ddae7715b245928881
tayaee <soonki.ji@gmail.com>
2023-05-13 03:27:22 +0000

    stream map

--- java8demo/src/main/java/com/example/stream/StreamExampleMap.java
+++ java8demo/src/main/java/com/example/stream/StreamExampleMap.java
@@ -6,7 +6,9 @@ import java.util.List;
 public class StreamExampleMap {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
-        int sumOfSquares = nums.stream().map(n -> n * n).reduce(0, Integer::sum);
+        int sumOfSquares = nums.stream()
+            .map(n -> n * n)
+            .reduce(0, Integer::sum);
         System.out.println(sumOfSquares);
     }
 }
